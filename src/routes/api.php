6b317fa42f742d8e6e418b4a8732321612aeaaa7
carlosdico <2746313+carlosdico@users.noreply.github.com>
2025-02-05 08:13:01 +0000

    Fix styling

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use carlosdico\BouncerApi\Controllers\BouncerController;
+use Illuminate\Support\Facades\Route;
 
 Route::prefix('bouncer')->middleware('auth:sanctum')->group(function () {
     Route::post('/assign-role', [BouncerController::class, 'assignRole']);
